1532a66b7a88d0358df53ef07575561b9b816ad4
Dorotea Serrelli <127234725+DoroteaSerrelli@users.noreply.github.com>
2024-04-19 15:02:07 +0200

    Script.sql caricamento prodotti database
    Si carica un file .sql per memorizzare nel database i prodotti venduti senza aver associato immagini in evidenza e galleria di immagini.

---
+++ Semilavorati/Codice/Database/Popolamento_prodotti_database.sql
@@ -0,0 +1,184 @@
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(1, 'Apple Watch SE', 'Apple Watch SE GPS Cassa 40mm in Alluminio Galassia con Cinturino Sport Galassia - S/M ha tutto per farsi amare!
+Modi efficaci per restare in contatto con chi vuoi. Più info sott’occhio quando ti alleni. Funzioni innovative per la salute e la sicurezza. 
+E abbinamenti di casse e cinturini a impatto ambientale neutro. 
+Apple Watch SE ha tutto quello che ti serve, a un prezzo che amerai.', 
+'+ CARBON NEUTRAL – Se vuoi un modello a impatto ambientale neutro, abbina Apple Watch SE (seconda generazione) al nuovo Sport Loop. Scopri di più sull’impegno di Apple per l’ambiente su apple.com/it/2030.3
+
++ FUNZIONI PER LA SALUTE E LA SICUREZZA – Chiama aiuto quando serve con “Rilevamento cadute”,4 “Rilevamento incidenti” e SOS emergenze.2 Scopri dati utili sulla tua salute, e ricevi una notifica se il battito cardiaco è irregolare,5 o se la frequenza è troppo alta o troppo bassa.
+
++ SEMPLICEMENTE COMPATIBILE – Tutto funziona in perfetta sintonia con i tuoi dispositivi e servizi Apple. Usa l’Apple Watch per sbloccare automaticamente il tuo Mac. Trova in un attimo i tuoi dispositivi. Paga con Apple Pay.6 Apple Watch SE richiede iPhone Xs o successivo con iOS 17 o successivo.
+
++ SWIMPROOF E STILOSO – Resistenza all’acqua fino a 50 metri.', 240.00, 'Prodotti elettronica', 'Smartwatch', 'Apple', 'Watch SE GPS', 1, 1, '', 110);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(2, 'Samsung Galaxy Watch6', 'Samsung Galaxy Watch6 Smartwatch Analisi del Sonno Ghiera Touch in Alluminio 40mm Graphite è pensato per monitorare e migliorare il benessere di tutti i giorni. Con le analisi approfondite e le rilevazioni della qualità del sonno Galaxy Watch6 ti aiuta a capire meglio il tuo corpo e le abitudini del riposo, supportandoti durante la giornata con alleneamenti pensati su misura per la tua salute.', 
+'Misura la composizione corporea con BioActiveSensor e tieni traccia dei tuoi progressi con oltre 90 differenti tipi di allenamento. Lo smartwatch, inoltre, rileva e notifica quando la frequenza cardiaca è alta o bassa.
+La cornice ora più sottile offre uno schermo più ampio, in modo da poter controllare con un solo sguardo tutti i dati relativi all\'allenamento o alla salute. Accoppia l\'orologio con uno smartphone Samsung Galaxy, accedi ai dati di benessere con l\'app Samsung Health e scatta foto di gruppo controllando la fotocamera dello smartphone da lontano.', 229.00, 'Prodotti elettronica', 'Smartwatch', 'Samsung', 'Galaxy Watch6', 1, 1, '', 102);
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(3, 'Xiaomi Redmi Note 13', 'Xiaomi Redmi Note 13 presenta un design veramente raffinato: le montature sottili infatti, aggiungono stile ed eleganza; inoltre, i suoi bordi piatti facilitano una presa precisa e confortevole.
+Lo straordinario display AMOLED, avente una frequenza di aggiornamento fino a 120Hz, garantisce che sia i video che i giochi appaiano fluidi. 
+Redmi Note 13 sfoggia una serie di fotocamere d\'élite, con un impressionante sensore principale fino a 108 MP. Questa combinazione garantisce immagini nitide anche se catturate da lontano.', 
+'Redmi Note 13 ha uno dei processori più richiesti, Snapdragon 685, che con la sua costruzione avanzata a 6nm migliora l\'efficienza e la potenza di utilizzo,
+permettendoti di lavorare con applicazioni complesse e giochi impegnativi. 
+Caratteristiche tecniche: 
+- dimensioni schermo: 16,9 cm (6.67");
+- Doppia SIM;
+- Android 13;
+- connessione 4G;
+- attacco USB tipo-C;
+- RAM: 8 GB;
+- Memoria: 256 GB;
+- Capacità batteria: 5000 mAh;
+- Colore : Nero', 229.90, 'Telefonia', 'Smartphone', 'Xiaomi', 'Redmi Note 13', 1, 1, '', 180);
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(4, 'Oppo A38', 'OPPO A38 4G offre un\'esperienza audio di alto livello, grazie alla presenza del doppio speaker stereo, dotato di tecnologia Ultra Volume Mode, che permette di alzare il volume di ciò che stiamo ascoltando fino al 300% (200% in chiamata).
+OPPO A38 4G offre prestazioni in grado di supportare anche gli utilizzi più intensivi, l’espansione della memoria RAM da 4GB (espandibile fino a 8) combinata alla potenza hardware/software garantiscono fluidità costante e velocità d’esecuzione. L’ampio display LCD da 6,56 pollici, con refresh rate a 90Hz rende qualsiasi contenuto immersivo e coinvolgente, ottimizzando l’efficienza energetica.',
+'OPPO A38 4G è dotato di una doppia fotocamera in alta risoluzione (Primaria 50MP, Profondità 2MP). Grazie alla presenza dell’ AI, gli scatti saranno sempre ottimizzati, anche nelle condizioni più complesse. Inoltre la fotocamera di profondità da 2MP, rende le foto in modalità ritratto ancora più dettagliate e d’impatto.
+OPPO A38 4G è dotata della tecnologia DYNAMIC COMPUTING ENGINE, realizzata in collaborazione con Google, che introduce un algoritmo intelligente di monitoraggio delle applicazioni in background e della memoria di sistema per rendere l’esperienza d’utilizzo sempre fluida e stabile. Inoltre, questa innovativa tecnologia, migliora il consumo della batteria e ne preserva la temperatura, anche sotto stress.
+Lo standard internazionale IP54 certifica la resistenza di un dispositivo di elettronica di consumo ad agenti esterni quali polveri ed acqua.
+Caratteristiche tecniche: 
+- Connessione : 4G;
+- AI Doppia fotocamera 50+2MP, Selfie 5MP;
+- Display 6.56” 90HZ LCD HD+;
+- Batteria : 5000mAh;
+- RAM : 4GB (Esp 1GB/2GB/4GB);
+- ROM : 128GB (esp1TB);
+- Colore : Glowing Gold', 129.99, 'Telefonia', 'Smartphone', 'Oppo', 'A38', 1, 1, '', 220);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(5, 'Lenovo IdeaCentre 3', 'Il desktop IdeaCentre 3 (AMD) è un modello tower compatto progettato con un elegante chassis grigio minerale che si adatta perfettamente a qualsiasi ambiente della casa. Offre le straordinarie prestazioni dei processori AMD Ryzen™ e vanta una scheda grafica integrata, grande capacità di memoria e storage e numerose porte, per soddisfare le esigenze di elaborazione quotidiane.',
+'- Lo chassis ultra compatto da 7,4 litri si adatta a qualsiasi ambiente per risparmiare spazio
+- Ricco di potenza, con CPU fino a Intel® di dodicesima generazione
+- Spazio di storage per tutti i tuoi videogame e file, inclusa un\'opzione con doppia unità disco fisso ed SSD
+- Scegli la modalità silenziosa o per le prestazioni usando la funzionalità Smart Power
+- Semplice connettività con Wi-Fi veloce e più porte anteriori', 599.90, 'Prodotti elettronica', 'PC', 'Lenovo', 'IdeaCentre 3', 1, 1, '', 132);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(6, 'Acer NITRO 50 N50-650 Tower', 'Dalle tonalità nere e accattivanti, è un PC da gaming predefinito e rifinito per offrire le massime prestazioni grazie al processore Intel® Core™ di 13a generazione, alla scheda grafica NVIDIA® GeForce RTX™ serie 40 e allo storage velocissimo per tutte le esigenze di caricamento.',
+'Gioca, riproduci in streaming e progetta come un professionista con il processore Intel® Core™ di 13a generazione e la nuova architettura ibrida ad alte prestazioni.
+Se associato al router giusto, il nuovissimo standard Wi-Fi 6E ti offre accesso alla massima velocità.
+Trasforma le cuffie o gli speaker in un sofisticato sistema audio surround a 360° con cui apprezzare al meglio il realismo dell\'audio 3D mentre giochi, guardi un film o ascolti musica.
+Caratteristiche specifiche:
+- Processore : Intel® Core™ i7 i7-13700F;
+- RAM : 16 GB DDR4-SDRAM;
+- ROM : 1,02 TB SSD;
+- Scheda grafica : NVIDIA GeForce RTX 4060;
+- Sistema operativo : Windows 11 Home;
+- Colore : Nero', 1399.00, 'Prodotti elettronica', 'PC', 'Acer', 'NITRO 50 N50-650', 1, 1, '', 73);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(7, 'Dyson Supersonic asciuga capelli', 'Asciugatura rapida senza danni da calore: Dyson Supersonic il phon che rivoluziona l\'asciugatura. Rapido, sicuro e adatto a tutti i tipi di capelli. Scopri la rivoluzione dello styling senza danni da calore.',
+'Caratteristiche specifiche:
+- Velocità motore: 110000 Giri/min;
+- Flusso d\'aria: 41 l/s;
+- Temperatura: 28 °C;
+- Lunghezza cavo: 2,8 m;
+- Potenza: 1600 W;
+- Larghezza: 97,8 mm;
+- Profondità: 288,3 mm;
+- Altezza: 77 mm;
+- Colore : Rosa, Rose gold', 1399.00, 'Piccoli elettrodomestici', '', 'Dyson', 'Supersonic', 1, 1, '', 45);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(8, 'Rowenta DW6330 Eco Intelligence Ferro da stiro', 'Con uno straordinario livello di prestazioni ed efficienza energetica, Rowenta Eco Intelligence offre fantastici risultati di stiratura risparmiando fino al 30% di energia*, con un design ecologico composto da materiale riciclato fino al 30%.',
+'Caratteristiche specifiche:
+- Tipo: Ferro da stiro a secco e a vapore;
+- Materiale piastra: Microsteam 400 HD soleplate;
+- Emissione concentrata vapore: 180 g/min;
+- Potenza ferro: 2500 W.', 54.90, 'Piccoli elettrodomestici', '', 'Rowenta', 'DW6330', 1, 1, '', 75);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(9, 'Ariete Handy Force - Scopa Elettrica con filo', 'Handy Force Ariete 2761 è la scopa elettrica con filo leggera e maneggevole, che si trasforma facilmente in un pratico aspirabriciole portatile per la pulizia di tutti gli ambienti e gli angoli della casa.
+La scopa elettrica con filo è potente e ideale per la pulizia di pavimenti di ogni genere, tappeti e moquette, grazie alla tecnologia ciclonica senza sacco, che consente di aspirare anche lo sporco più ostinato.',
+'Con una sola mossa la scopa elettrica Handy Force si trasforma in un aspirabriciole portatile con beccuccio perfetto per la pulizia di angoli e fessure difficili da raggiungere, mensole e piani.
+Utilizzando l\'apposito accessorio tessuti, con Handy Force si possono aspirare in un istante anche divani, poltrone e cuscini. Handy Force non è solamente pratica e versatile da utilizzare, 
+ma anche semplicissima da pulire: il serbatoio infatti può essere svuotato in un istante direttamente sopra alla pattumiera e il filtro Hepa, che garantisce la massima aspirazione di polvere e sporcizia, può essere lavato direttamente sotto l\'acqua corrente, per garantirti sempre la miglior performance di aspirazione.', 46.00, 'Piccoli elettrodomestici', '', 'Ariete', 'Handy Force 2761', 1, 1, '', 68);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(10, 'Bosch Lavatrice a carica frontale', 'Lavatrice silenziosa ed efficiente, con motore EcoSilence Drive e sistema di riconoscimento del carico ActiveWater.',
+'- Capacità : 7kg.
+- AcquaStop : protezione antiallagamento', 319.90, 'Grandi elettrodomestici', '', 'Bosch', 'WAE20037IT Serie 2', 1, 1, '', 112);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(11, 'Ignis frigorifero con congelatore', 'Questo elettrodomestico elegante e alla moda non è solo un piacere per gli occhi, ma anche una potenza di innovazione e tecnologia. 
+Con il suo interno spazioso, avrai un sacco di spazio per conservare tutti i tuoi cibi e bevande preferiti. Esso dispone di ripiani regolabili, in modo da poter personalizzare lo spazio in base alle tue esigenze.
+Inoltre, grazie al suo sistema di raffreddamento avanzato, puoi essere sicuro che il tuo cibo rimarrà fresco e delizioso più a lungo.',
+'- Capacità netta totale: 213 L.
+- Classe climatica: N-ST;
+- Emissione acustica: 41 dB.
+- Capacità netta frigorifero: 171 L, 
+- Numero di ripiani frigorifero: 4,
+- Numero di cassetti per verdura: 1.
+- Capacità netta congelatore: 42 L,
+- Capacità di congelamento: 2 kg/24h.
+- Classe efficienza energetica: F,
+- Consumo energetico annuo: 221 kWh.
+- Colore del prodotto: Argento', 279.90, 'Grandi elettrodomestici', '', 'Ignis', 'IG55TM 4110 A', 1, 1, '', 50);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(12, 'HP 15s-fq5040nl', 'Un laptop leggero, perfetto per il lavoro e il divertimento ovunque.
+Lavora e divertiti, ovunque ti portino i tuoi impegni e le tue passioni, grazie al laptop HP da 15,6" con un design leggero e una durata prolungata della batteria. Goditi il comfort di uno schermo antisfarfallio e sfrutta le prestazioni affidabili del processore Intel® Core™.
+In più, è realizzato pensando all\'ambiente, con registrazione EPEAT® Silver e certificazione ENERGY STAR®.',
+'- Processore: Intel® Core™ i5 i5-1235U;
+- Dimensioni schermo: 39,6 cm (15.6") Full HD;
+- RAM: 8 GB DDR4-SDRAM;
+- ROM: 512 GB SSD;
+- Wi-Fi: 5 (802.11ac);
+- Sistema operativo : Windows 11 Home;
+- Colore: Argento', 450.00, 'Prodotti elettronica', 'PC', 'HP', '15s-fq5040nl', 1, 1, '', 80);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(13, 'Amazfit T-Rex 2', 'Smartwatch robusto per attività all\'aperto con GPS.', 
+'- Super resistente dentro e fuori
+- Conquista l\'inesplorato : resistente a bassissime temperature;
+- Riconoscimento intelligente degli esercizi dell\'allenamento contro resistenza', 160.00, 'Prodotti elettronica', 'Smartwatch', 'Amazfit', 'T-Rex 2', 1, 1, '', 10);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(14, 'Samsung Galaxy Tab A7 Lite', 'Tutto l\'intrattenimento dei tuoi film e giochi preferiti su un ampio display da 8,7 pollici. Il sottilissimo spessore delle cornici offre un rapporto schermo-corpo più elevato senza aumentare le dimensioni del tablet, e la sua forma compatta evita l\'affaticamento delle mani, anche dopo ore di gioco.', 
+'- Dimensioni schermo: 8.7''
+- Capacità memoria interna: 64  GB;
+- RAM installata: 4  GB,
+- Capacità della batteria: 5100 mAh;
+- Risoluzione fotocamera posteriore (numerico): 8  MP;
+- Colore del prodotto: Grigio.', 110.00, 'Telefonia', 'Tablet', 'Samsung', 'SM-T220N', 1, 1, '', 70);
+
+
+INSERT INTO prodotto(CODICEPRODOTTO, NOME, TOPDESCRIZIONE, DETTAGLI, PREZZO, CATEGORIA, SOTTOCATEGORIA,
+MARCA, MODELLO, INCATALOGO, INVETRINA, TOPIMAGE, QUANTITà) 
+VALUES(15, 'Lenovo Tab M11', 'Esperienza di gioco straordinaria e apprendimento smarter.', 
+'- Dimensioni schermo: 11'';
+- Capacità memoria interna: 128 GB;
+- RAM installata: 4 GB;
+- Generazione di reti mobili: 4G;
+- Capacità della batteria: 7040 mAh;
+- Famiglia processore: Mediatek;
+- Risoluzione fotocamera posteriore (numerico): 8 MP;
+- Sistema operativo incluso: Android 13', 119.00, 'Telefonia', 'Tablet', 'Lenovo', 'TB330XU', 1, 1, '', 40);

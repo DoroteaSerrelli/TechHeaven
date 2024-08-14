3180596efac620e725fca4fc77739dfddebc54f9
DoroteaSerrelli <d.serrelli1@studenti.unisa.it>
2024-08-14 18:28:15 +0200

    Fix per l'operazione di aggiornamento di un indirizzo in updateAddressBook

--- Sistema/src/main/java/application/AutenticazioneService/AutenticazioneServiceImpl.java
+++ Sistema/src/main/java/application/AutenticazioneService/AutenticazioneServiceImpl.java
@@ -36,7 +36,7 @@ import storage.AutenticazioneDAO.*;
  * */
 
 public class AutenticazioneServiceImpl implements AutenticazioneService{
-	
+
 	/**
 	 * Il metodo effettua l'autenticazione dell'utente: verifica la corrispondenza 
 	 * tra le credenziali inserite (viene effettuato l'hash della password fornita) 
@@ -61,8 +61,8 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 		ArrayList<Ruolo> roles = (new RuoloDAODataSource()).doRetrieveByKey(username);
 		return new ProxyUtente(username, userReal.getPassword(), roles);
 	}
-	
-	
+
+
 	/**
 	 * Il metodo effettua la reimpostazione della password dell'utente: prima, verifica la corrispondenza 
 	 * tra le credenziali inserite e le credenziali dell'utente memorizzate nel database, poi, 
@@ -89,11 +89,11 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 				//hashing della nuova password
 				userReal.setPasswordToHash(newPassword);
 				userDAO.doResetPassword(username, userReal.getPassword());
-				
+
 			}
 		}
 	}
-	
+
 	/**
 	 * Il metodo effettua la modifica del numero di telefono e dell'email dell'utente.
 	 * @param user : l'utente che richiede la modifica della propria email/
@@ -114,7 +114,7 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 		UtenteDAODataSource userDAO = new UtenteDAODataSource();
 		ClienteDAODataSource profileDAO = new ClienteDAODataSource();
 		Utente userReal;
-		
+
 		if(information.equalsIgnoreCase("EMAIL")) {
 			if((userReal = userDAO.doRetrieveFullUserByKey(user.getUsername())) == null)
 				throw new ProfiloInesistenteException("Errore nel recupero delle informazioni relative"
@@ -130,7 +130,7 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 				}
 			}
 		}
-		
+
 		if(information.equalsIgnoreCase("TELEFONO")) {
 			if((userReal = userDAO.doRetrieveFullUserByKey(user.getUsername())) == null)
 				throw new ProfiloInesistenteException("Errore nel recupero delle informazioni relative"
@@ -148,8 +148,8 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 		}
 		throw new InformazioneDaModificareException("Selezionare un'informazione del profilo utente da modificare");
 	}
-	
-	
+
+
 	/**
 	 * Il metodo effettua aggiunta/rimozione/aggiornamento
 	 * di un indirizzo di spedizione dell'utente.
@@ -164,12 +164,12 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 	 * @throws ModificaIndirizzoException 
 	 * @throws InformazioneDaModificareException 
 	 * */
-	
+
 	@Override
 	public ProxyUtente updateAddressBook(ProxyUtente user, String information, Indirizzo updatedData) throws UtenteInesistenteException, IndirizzoEsistenteException, FormatoIndirizzoException, SQLException, ModificaIndirizzoException, InformazioneDaModificareException {
 		UtenteDAODataSource userDAO = new UtenteDAODataSource();
 		IndirizzoDAODataSource addressDAO = new IndirizzoDAODataSource();
-		
+
 		if(information.equalsIgnoreCase("AGGIUNGERE-INDIRIZZO")) {
 			if(userDAO.doRetrieveFullUserByKey(user.getUsername()) == null)
 				throw new UtenteInesistenteException("Errore nel recupero delle informazioni relative"
@@ -181,13 +181,13 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 				else {
 					if(!Indirizzo.checkValidate(updatedData))
 						throw new FormatoIndirizzoException("Formato del nuovo indirizzo non valido");
-					
+
 					addressDAO.doSave(updatedData, user.getUsername());
 					return user;
 				}
 			}
 		}
-		
+
 		if(information.equalsIgnoreCase("RIMUOVERE-INDIRIZZO")) {
 			if(userDAO.doRetrieveFullUserByKey(user.getUsername()) == null)
 				throw new UtenteInesistenteException("Errore nel recupero delle informazioni relative"
@@ -199,31 +199,32 @@ public class AutenticazioneServiceImpl implements AutenticazioneService{
 				else {
 					if(!Indirizzo.checkValidate(updatedData))
 						throw new FormatoIndirizzoException("Formato dell'indirizzo non valido");
-					
+
 					addressDAO.doDeleteAddress(updatedData.getIDIndirizzo(), user.getUsername());
 					return user;
 				}
 			}
 		}
-		
+
 		if(information.equalsIgnoreCase("AGGIORNARE-INDIRIZZO")) {
 			if(userDAO.doRetrieveFullUserByKey(user.getUsername()) == null)
 				throw new UtenteInesistenteException("Errore nel recupero delle informazioni relative"
 						+ "all'utente");
 			else {
 				ArrayList<Indirizzo> addresses = addressDAO.doRetrieveAll(user.getUsername());
-				if(!addresses.contains(updatedData))
-					throw new ModificaIndirizzoException("Indirizzo inserito non associato all'utente");
-				else {
-					if(!Indirizzo.checkValidate(updatedData))
-						throw new FormatoIndirizzoException("Formato del nuovo indirizzo non valido");
-					
-					addressDAO.doUpdateAddress(updatedData, user.getUsername());
-					return user;
-				}
+				for(Indirizzo search_ind: addresses){
+					if(search_ind.getIDIndirizzo() == updatedData.getIDIndirizzo()){
+						if(!Indirizzo.checkValidate(updatedData))
+							throw new FormatoIndirizzoException("Formato del nuovo indirizzo non valido");
+
+						addressDAO.doUpdateAddress(updatedData, user.getUsername());
+						return user;
+					}
+				}                            
+				throw new ModificaIndirizzoException("Indirizzo inserito non associato all'utente");			
 			}
 		}
-		
+
 		throw new InformazioneDaModificareException("Selezionare un'informazione del profilo utente da modificare");
 	}
 

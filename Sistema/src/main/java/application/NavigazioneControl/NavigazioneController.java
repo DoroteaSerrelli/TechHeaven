bb21f75c888e714abaf29d5f86e69ca9f2706f8c
Raffaella <131981162+SabatinoRaffaella@users.noreply.github.com>
2024-08-31 15:44:27 +0200

    Ultime Redirects Corrette

--- Sistema/src/main/java/application/NavigazioneControl/NavigazioneController.java
+++ Sistema/src/main/java/application/NavigazioneControl/NavigazioneController.java
@@ -44,7 +44,7 @@ public class NavigazioneController extends HttpServlet {
         // For example, you could return an error response or redirect the user
             if(keyword==null){
                 System.out.println("keyword");
-                response.sendRedirect("index.jsp");
+                response.sendRedirect(request.getContextPath() + "index.jsp");
                 return;
             }
         }  
@@ -69,7 +69,7 @@ public class NavigazioneController extends HttpServlet {
         
         if(searchType==null){
             System.out.println("SONO QUIIIII");
-            response.sendRedirect("index.jsp");
+            response.sendRedirect(request.getContextPath() + "index.jsp");
             return;
             
         }

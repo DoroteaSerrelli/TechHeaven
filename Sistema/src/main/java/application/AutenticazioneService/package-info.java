f6dfb7f1638f61e4902bf2ec205e7ae73da55c71
Dorotea Serrelli <d.serrelli1@studenti.unisa.it>
2024-09-17 14:24:35 +0200

    Info generali sul package AutenticazioneService

--- Sistema/src/main/java/application/AutenticazioneService/package-info.java
+++ Sistema/src/main/java/application/AutenticazioneService/package-info.java
@@ -1,1 +1,14 @@
+/**
+ * Questo pacchetto contiene le classi principali dell'applicazione
+ * per effettuare le seguenti operazioni per un utente:
+ * 
+ * - autenticazione nel sistema;
+ * - reimpostazione della password;
+ * - aggironamento del profilo personale;
+ * - aggiornamento della rubrica degli indirizzi di spedizione.
+ *
+ * @author Dorotea Serrelli
+ */
+
+
 package application.AutenticazioneService;

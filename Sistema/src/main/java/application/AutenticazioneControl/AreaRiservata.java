02e717ab7b657de15a9aecfa5e44c73f46d5503e
DoroteaSerrelli <d.serrelli1@studenti.unisa.it>
2024-10-04 20:50:37 +0200

    Organizzazione commenti Javadoc

--- Sistema/src/main/java/application/AutenticazioneControl/AreaRiservata.java
+++ Sistema/src/main/java/application/AutenticazioneControl/AreaRiservata.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package application.AutenticazioneControl;
 
 import application.RegistrazioneService.Indirizzo;
 import application.RegistrazioneService.ProxyUtente;
-import application.RegistrazioneService.Utente;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -20,99 +14,102 @@ import javax.servlet.http.HttpServletResponse;
 import storage.AutenticazioneDAO.IndirizzoDAODataSource;
 
 /**
+ * La servlet controlla l'accesso ad una pagina riservata agli utenti autenticati.
  *
  * @author raffa
  */
+
 public class AreaRiservata extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ProxyUtente u = (ProxyUtente) request.getSession().getAttribute("user");
-        if (u==null || u.getUsername().equals("")) {
-           response.sendRedirect(request.getContextPath() + "/Autenticazione");
-           return;
-        }    
-        // Retrieve data from request or session if needed
-        ArrayList<Indirizzo> indirizzi = (ArrayList<Indirizzo>) request.getAttribute("Indirizzi");
-        if(indirizzi==null){
-            try {
-                loadUserAddresses(request, u);
-            } catch (SQLException ex) {
-                Logger.getLogger(AreaRiservata.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        // Forward to JSP     
-        request.getRequestDispatcher("/protected/cliente/AreaRiservata.jsp").forward(request, response);
-    }
+	/**
+	 * serialVersionUID : È un campo statico finale a lungo raggio utilizzato 
+	 * per la serializzazione dell'oggetto.
+	 */
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ProxyUtente u = (ProxyUtente) request.getSession().getAttribute("user");
-        if (u==null || u.getUsername().equals("")) {
-           response.sendRedirect(request.getContextPath() + "/Autenticazione");
-           return;
-        }    
-        // Retrieve data from request or session if needed
-        ArrayList<Indirizzo> indirizzi = (ArrayList<Indirizzo>) request.getAttribute("Indirizzi");
-        if(indirizzi==null){
-            try {
-                loadUserAddresses(request, u);
-            } catch (SQLException ex) {
-                Logger.getLogger(AreaRiservata.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        // Forward to JSP
-       request.getRequestDispatcher("/protected/cliente/AreaRiservata.jsp").forward(request, response);
-    }
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	/**
+	 * Gestisce le richieste HTTP GET alla servlet. 
+	 *
+	 * - Verifica l'autenticazione: Controlla se l'utente è autenticato, cercando l'oggetto `ProxyUtente` nella sessione HTTP. 
+	 *      Se l'utente non è autenticato, reindirizza alla pagina di autenticazione.
+	 * - Recupero degli indirizzi: Se l'utente è autenticato, verifica se gli indirizzi dell'utente sono già stati recuperati e memorizzati nella richiesta. 
+	 *      Se gli indirizzi non sono presenti, li carica dal database utilizzando il metodo `loadUserAddresses`.</li>
+	 * - Inoltro alla JSP: Inoltra la richiesta alla pagina JSP `/protected/cliente/AreaRiservata.jsp`, che visualizzerà l'area riservata dell'utente, inclusi i suoi indirizzi.</li>
+	 * 
+	 * @param request : La richiesta HTTP inviata dal client.
+	 * @param response : La risposta HTTP che verrà inviata al client.
+	 * @throws ServletException : Se si verifica un errore specifico della servlet.
+	 * @throws IOException : Se si verifica un errore di input/output.
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ProxyUtente u = (ProxyUtente) request.getSession().getAttribute("user");
+		if (u==null || u.getUsername().equals("")) {
+			response.sendRedirect(request.getContextPath() + "/Autenticazione");
+			return;
+		}    
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-    
-    
-    private void loadUserAddresses(HttpServletRequest request, ProxyUtente u) throws SQLException {
-        IndirizzoDAODataSource indDAO = new IndirizzoDAODataSource();
-        ArrayList<Indirizzo> indirizzi = indDAO.doRetrieveAll("Indirizzo.via", u.getUsername());
-        request.setAttribute("Indirizzi", indirizzi); 
-       
-    }
+		//Recupero degli indirizzi dell'utente
+		ArrayList<Indirizzo> indirizzi = (ArrayList<Indirizzo>) request.getAttribute("Indirizzi");
+		if(indirizzi==null){
+			try {
+				loadUserAddresses(request, u);
+			} catch (SQLException ex) {
+				Logger.getLogger(AreaRiservata.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+
+		request.getRequestDispatcher("/protected/cliente/AreaRiservata.jsp").forward(request, response);
+	}
+
+	/**
+	 * Gestisce le richieste HTTP con metodo POST.
+	 *
+	 * Controlla se l'utente è autenticato, recuperando l'oggetto `ProxyUtente` dalla sessione.
+	 * Se l'utente non è autenticato, reindirizza alla pagina di autenticazione.
+	 * Se l'utente è autenticato, recupera gli indirizzi dell'utente se non sono già presenti nella richiesta.
+	 * Infine, inoltra la richiesta alla pagina JSP `/protected/cliente/AreaRiservata.jsp`.
+	 *
+	 * @param request : la richiesta servlet
+	 * @param response : la risposta servlet
+	 * @throws ServletException : se si verifica un errore specifico della servlet
+	 * @throws IOException : se si verifica un errore di I/O
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ProxyUtente u = (ProxyUtente) request.getSession().getAttribute("user");
+		if (u==null || u.getUsername().equals("")) {
+			response.sendRedirect(request.getContextPath() + "/Autenticazione");
+			return;
+		}   
+
+		//Recupero degli indirizzi dell'utente
+		ArrayList<Indirizzo> indirizzi = (ArrayList<Indirizzo>) request.getAttribute("Indirizzi");
+		if(indirizzi==null){
+			try {
+				loadUserAddresses(request, u);
+			} catch (SQLException ex) {
+				Logger.getLogger(AreaRiservata.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+
+		request.getRequestDispatcher("/protected/cliente/AreaRiservata.jsp").forward(request, response);
+	}
+
+	/**
+	 * Carica gli indirizzi dell'utente dal database.
+	 *
+	 * @param request : la richiesta servlet
+	 * @param utente : l'oggetto `ProxyUtente` che rappresenta l'utente
+	 * @throws SQLException : se si verifica un errore con il database
+	 */ 
+	private void loadUserAddresses(HttpServletRequest request, ProxyUtente u) throws SQLException {
+		IndirizzoDAODataSource indDAO = new IndirizzoDAODataSource();
+		ArrayList<Indirizzo> indirizzi = indDAO.doRetrieveAll("Indirizzo.via", u.getUsername());
+		request.setAttribute("Indirizzi", indirizzi); 
+
+	}
 }

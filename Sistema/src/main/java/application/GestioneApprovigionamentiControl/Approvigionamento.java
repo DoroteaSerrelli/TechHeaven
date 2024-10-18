7a61f700e4796db592f0c5a90894f1efc058a222
DoroteaSerrelli <d.serrelli1@studenti.unisa.it>
2024-10-18 19:55:16 +0200

    Rimozione metodo processRequest e getServletInfo

--- Sistema/src/main/java/application/GestioneApprovigionamentiControl/Approvigionamento.java
+++ Sistema/src/main/java/application/GestioneApprovigionamentiControl/Approvigionamento.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package application.GestioneApprovigionamentiControl;
 
 import application.GestioneApprovvigionamenti.RichiestaApprovvigionamento;
@@ -23,34 +19,10 @@ import javax.servlet.http.HttpServletResponse;
 public class Approvigionamento extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Approvigionamento</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Approvigionamento at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-
-    private int perPage=50;
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int perPage=50;
     
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -93,8 +65,7 @@ public class Approvigionamento extends HttpServlet {
         }
         String error = (String)request.getSession().getAttribute("error");
         request.getSession().removeAttribute("error");
-        request.setAttribute("error", error);
-              
+        request.setAttribute("error", error);       
     }
 
     /**
@@ -108,17 +79,7 @@ public class Approvigionamento extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        doGet(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
